f9f49482b73d661a142ddae6f792a0a7401bbfcf
A. R. Shajii <ariya.r.shajii@gmail.com>
2012-10-27 23:38:18 -0400

    Deleted useless data fields.

--- src/xi/nodes/Node.java
+++ src/xi/nodes/Node.java
@@ -3,18 +3,7 @@ package xi.nodes;
 import xi.datatypes.DataType;
 
 public abstract class Node {
-	
-	protected Node left;
-	protected Node right;
 
 	public abstract DataType evaluate();
-
-	public void setLeft(Node node) {
-		left = node;
-	}
-
-	public void setRight(Node node) {
-		right = node;
-	}
 	
 }
