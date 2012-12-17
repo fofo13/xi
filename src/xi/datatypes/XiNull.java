ff8aa8f9c7a33da9f700ea4675e4d281e5a03a60
A. R. Shajii <ariya.r.shajii@gmail.com>
2012-12-16 19:13:04 -0500

    Made XiNull a final class.

--- src/xi/datatypes/XiNull.java
+++ src/xi/datatypes/XiNull.java
@@ -1,44 +1,44 @@
 package xi.datatypes;
 
+public final class XiNull extends DataType {
 
-public class XiNull extends DataType {
-	
 	private static final XiNull instance = new XiNull();
-	
-	private XiNull() {}
-	
+
+	private XiNull() {
+	}
+
 	public static XiNull instance() {
 		return instance;
 	}
-	
+
 	@Override
 	public boolean isEmpty() {
 		return true;
 	}
-	
+
 	@Override
 	public int compareTo(DataType other) {
-		return other instanceof XiNull ? 0 : -1;
+		return (other instanceof XiNull) ? 0 : -1;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "null";
 	}
-	
+
 	@Override
 	public int length() {
 		return 0;
 	}
-	
+
 	@Override
 	public boolean equals(Object o) {
 		return o instanceof XiNull;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return Boolean.FALSE.hashCode();
 	}
-	
+
 }
