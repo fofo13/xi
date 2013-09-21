eb2d6bbc6c391fb04afd4bb8924f1b98be2164a4
alfonso <fofosav85@hotmail.it>
2013-09-21 15:54:39 +0200

    XiProgramTest
    only one  TestMethod to implement

--- src/org/xiscript/xi/XiTest.java
+++ src/org/xiscript/xi/XiTest.java
@@ -10,12 +10,21 @@ public class XiTest extends Xi{
 
 	@Test
 	public void testRunFromFile() {
-		String s=new String();
-		s="C:\\Users\\alfonso\\Desktop\\input.txt";
-	
+		String s1=new String();
+		s1="C:\\Users\\alfonso\\Desktop\\input.txt";
 		
-		runFromFile(s);
-		assertNotNull(s);
+		runFromFile(s1);
+		assertNotNull(s1);
+		/*in the  first  case s1 is  a  real file input for Xi project*/
+		
+		String s2=new String();
+		s2="C:\\Users\\alfonso\\Desktop\\fittizio.txt";		
+		runFromFile(s2);
+		assertNotNull(s2);		
+		
+		/*in this  case instead s2 not is  a  real input file  for xi project
+		 * it's necessary to throw an exception into runFromFile method and cover
+		 * all line of  it*/
 		
 	}
 
--- src/org/xiscript/xi/core/XiProgramTest.java
+++ src/org/xiscript/xi/core/XiProgramTest.java
@@ -4,9 +4,15 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 import org.junit.Test;
+import org.xiscript.xi.datatypes.DataType;
+import org.xiscript.xi.exceptions.ErrorHandler;
+import org.xiscript.xi.exceptions.ErrorHandler.ErrorType;
 
+@SuppressWarnings("unused")
 public class XiProgramTest{
 
 	@Test
@@ -32,17 +38,86 @@ public class XiProgramTest{
 
 	@Test
 	public void testScope() {
-		fail("Not yet implemented");
+		String s=new String();
+		s="C:\\Users\\alfonso\\Desktop\\input.txt";
+		
+		File f= new File(s);
+		
+		XiProgram env;
+		env=XiProgram.readFrom(f);
+		assertNotNull(env.scope());
+		assertTrue(env.scope() instanceof VariableCache);
+		
 	}
 
 	@Test
-	public void testRun() {
-		fail("Not yet implemented");
+	public void testRun() throws FileNotFoundException {
+		String s=new String();
+		s="C:\\Users\\alfonso\\Desktop\\input.txt";
+		
+		File f= new File(s);
+		
+		XiProgram env;
+		env=new XiProgram(f);
+		assertNotNull(env.run());
+		assertTrue(env.run() instanceof DataType);
+		
 	}
 
 	@Test
-	public void testWriteTo() {
-		fail("Not yet implemented");
+	public void testWriteTo() throws IOException {
+		String s=new String();
+		s="C:\\Users\\alfonso\\Desktop\\input.txt";
+		
+		File f= new File(s);
+		
+		XiProgram env =new XiProgram(f);
+		
+		String s2=new String();
+		s2="C:\\Users\\alfonso\\Desktop\\test1.txt";
+		
+		File f2= new File(s2);
+		env.writeTo(f2);
+		
+		String s3=new String();
+		s3="C:\\Users\\alfonso\\Desktop\\test2.txt";
+		
+		File f3= new File(s3);
+		env.writeTo(f3);
+
+		String a=new String();
+		String b=new String();
+            // open file
+            FileReader filein = new FileReader("C:\\Users\\alfonso\\Desktop\\test1.txt");
+            
+            int next;
+        
+            do {
+                next = filein.read(); // read next char
+                
+                if (next != -1) { 
+                    a= a + (char)next;
+                				}
+            	} while (next != -1);
+        	
+        	filein.close(); // close file
+
+            // open file
+            FileReader filein2 = new FileReader("C:\\Users\\alfonso\\Desktop\\test2.txt");
+            
+            
+        
+            do {
+                next = filein2.read(); 
+                if (next != -1) { 
+                    b= b + (char)next;
+                				}
+            	} while (next != -1);
+            filein2.close(); 
+
+		assertEquals(a,b);
+		
+		
 	}
 
 	@Test
