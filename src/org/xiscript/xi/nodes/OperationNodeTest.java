babcf0b0959cd7ed30dd282426b7dd408446104e
alfonso <fofosav85@hotmail.it>
2013-09-25 01:45:28 +0200

    OperationNodeTest with some problem

--- src/org/xiscript/xi/nodes/OperationNodeTest.java
+++ src/org/xiscript/xi/nodes/OperationNodeTest.java
@@ -2,7 +2,11 @@ package org.xiscript.xi.nodes;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import org.junit.Test;
+import org.xiscript.xi.operations.BuiltInOperation;
 
 public class OperationNodeTest {
 
@@ -10,10 +14,12 @@ public class OperationNodeTest {
 	public void testHashCode() {
 		fail("Not yet implemented");
 	}
-
 	@Test
 	public void testOperationNodeListOfNodeOperation() {
-		fail("Not yet implemented");
+		String sb=new String("-");
+		Node node = new OperationNode(BuiltInOperation.parse(sb));
+		assertTrue(node instanceof OperationNode);
+	/*I don't understand  here  what  kind of  method junit4 want test!!! */
 	}
 
 	@Test
@@ -33,11 +39,27 @@ public class OperationNodeTest {
 
 	@Test
 	public void testOp() {
-		fail("Not yet implemented");
+		String sb=new String("-");
+		Node node=new OperationNode(BuiltInOperation.parse(sb));
+		assertNotNull(((OperationNode) node).op());
+		assertEquals(((OperationNode) node).op().toString(),"-");
 	}
 
 	@Test
 	public void testAddChild() {
+/*here i  can't assert nothing because addChild method  return void*/
+	/*	String sb=new String("-");
+		Node node=new OperationNode(BuiltInOperation.parse(sb));
+		Queue<Character> source = new LinkedList<Character>();
+		String n1=new String("12");
+		//System.out.print(input);
+		for (int i=0; i<n1.length() ;i++){
+			source.add(n1.charAt(i));
+		}
+		//Node x=readNum(source);
+		
+		node.addChild(Node(n1));
+		*/
 		fail("Not yet implemented");
 	}
 
